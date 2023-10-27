2bd19a156a884a472caf35a26e9ff5105f1ce8e8
taysa-fernandes <taysamirelly34@gmail.com>
2023-10-27 15:46:19 -0300

    correção de alguns bugs

--- avaliacao/urls.py
+++ avaliacao/urls.py
@@ -19,7 +19,7 @@ from core.views import *
 urlpatterns = [
     path("alunos/", StudentView.as_view(), name="student"),
     path("edit-student/<int:pk>", EditStudentView.as_view(), name="edit_student"),
-    path('criar-aluno/', CreateStudentView.as_view(), name='criar_aluno'),
+    path('criar_aluno/', CreateStudentView.as_view(), name='criar_aluno'),
     path('excluir-aluno/<int:pk>/', DeleteStudentView.as_view(), name='excluir_aluno'),
     path('student/<int:pk>/', StudentDetailView.as_view(), name='detalhes_aluno'),
 ]
